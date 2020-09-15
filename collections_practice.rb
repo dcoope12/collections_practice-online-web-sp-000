5ddcad0265bf2b1a5a5b6f88b3bbeba1b95053b2
Devin Cooper <dcoope12@asu.edu>
2020-09-15 04:43:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,10 @@ def sort_array_asc(integers)
 integers.sort
 end
 
-def sort_array_desc()
+def sort_array_desc(integers)
   integers.sort{|num1, num2| num2 <=> num1}
-end 
+end
+
+def sort_array_char_count(strings)
+strings.sort{|string1, string2| string1.length <=> string2.length}
+end
